15a8050238d2a5ddeb8a016a0749a10980b2ac41
YuBin <YuBin>
2023-06-07 08:54:59 +0800

    master commit1

---
+++ src/main/java/com/ybcoder/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.ybcoder.git;
+
+/**
+ * @author yubin
+ * @project git-test
+ * @date 2023/6/7
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("idea git version01");
+    }
+}
